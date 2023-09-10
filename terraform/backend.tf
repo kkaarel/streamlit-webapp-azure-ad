120b443b3315123b8b87145927a32535d04c4e5b
kkaarel <informations@hotmail.com>
2023-09-10 14:06:27 +0300

    backend access

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,8 @@ backend "azurerm" {
   container_name       = "tfstatestreamlitappad"
   key                  = "terraform.tfstate"
   storage_account_name = "kkkaarel"
+  use_oidc             = true
+
 
 
   }
