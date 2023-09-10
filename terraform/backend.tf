4f38d6fe2959410e73a64b0d24a940d896ca61d2
kkaarel <informations@hotmail.com>
2023-09-10 21:26:57 +0300

    resourcegroup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "kkaarel_dev001"
     container_name       = "tfstatestreamlitappad"
     key                  = "terraform.tfstate"
     storage_account_name = "kkkaarel"
