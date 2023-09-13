d3fed5fb180c0e5c98a0ed80c88e5240d6e68f93
kkaarel <informations@hotmail.com>
2023-09-13 21:22:37 +0300

    git integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,8 +100,11 @@ resource "azurerm_linux_web_app" "app" {
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id = azurerm_linux_web_app.app.id
   // There is a folder directed, not the whole branch
-  //repo_url               = "https://github.com/kkaarel/streamlit-webapp-azure-ad/tree/dev/streamlit"
-  //branch                 = "dev"
+  github_action_configuration {
+    generate_workflow_file = true
+  }
+  repo_url               = "https://github.com/kkaarel/streamlit-webapp-azure-ad/tree/dev/streamlit"
+  branch                 = "dev"
   use_manual_integration = true
   use_mercurial          = false
 }
