db740ba31e1d6aec9d485775eb54e2fc8d6d4d2b
kkaarel <informations@hotmail.com>
2023-09-16 13:03:11 +0300

    added comments to the code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,30 @@
-//resource group is created before hand
-//storage account and container are created before hand for state files
-
-
+// This data block retrieves Azure client configuration.
 data "azurerm_client_config" "current" {
 
 }
 
-
+// This data block retrieves information about the Azure resource group defined by var.RESOURCE_GROUP_NAME.
 data "azurerm_resource_group" "main" {
   name = var.RESOURCE_GROUP_NAME
-
 }
 
+// This archive_file block creates a ZIP archive from the "./streamlit" directory.
 data "archive_file" "app" {
   type        = "zip"
   source_dir  = "./streamlit"
   output_path = var.archive_file_streamlit
 }
 
-
-
-
-
+// This resource block creates an Azure Storage Account for Terraform state files.
 resource "azurerm_storage_account" "tfstate" {
   name                     = var.STORAGE_ACCOUNT_NAME
   resource_group_name      = data.azurerm_resource_group.main.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
 }
 
-
-
-
+// This resource block creates an Azure Service Plan.
 resource "azurerm_service_plan" "streamlit" {
   name                = "ASP-${var.project}-${terraform.workspace}"
   location            = "West Europe"
@@ -47,11 +38,9 @@ resource "azurerm_service_plan" "streamlit" {
       tags
     ]
   }
-
 }
 
-
-
+// This resource block creates an Azure Linux Web App.
 resource "azurerm_linux_web_app" "app" {
   name                = "WEBAPP-${var.project}"
   location            = var.location
@@ -63,7 +52,6 @@ resource "azurerm_linux_web_app" "app" {
     type = "SystemAssigned"
   }
 
-
   auth_settings_v2 {
     auth_enabled           = true
     require_authentication = true
@@ -71,7 +59,7 @@ resource "azurerm_linux_web_app" "app" {
     unauthenticated_action = "RedirectToLoginPage"
 
     active_directory_v2 {
-      // The application is pre created from Azure, this is for security puropses so that the deployment script would only have rights to create resources.
+      // The application is pre-created in Azure; this setting enhances security.
       client_id                   = var.CLIENT_ID_AD
       client_secret_setting_name  = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
       tenant_auth_endpoint        = "https://sts.windows.net/${var.ARM_TENANT_ID}/v2.0"
@@ -80,14 +68,11 @@ resource "azurerm_linux_web_app" "app" {
 
     login {
       token_store_enabled = true
-
     }
   }
 
   site_config {
     app_command_line = "python -m streamlit run app.py  --server.port 8000 --server.address 0.0.0.0"
-
-
 
     application_stack {
       python_version = "3.10"
@@ -96,24 +81,23 @@ resource "azurerm_linux_web_app" "app" {
 
   app_settings = {
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = 1
-    # Add other app settings as needed
+    // Add other app settings as needed
   }
 }
 
+// This local block defines a command for publishing code to the Azure Web App (Linux).
 locals {
-    publish_code_command_linux = "az webapp deployment source config-zip --resource-group ${azurerm_linux_web_app.app.resource_group_name} --name ${azurerm_linux_web_app.app.name} --src ${var.archive_file_streamlit}"
-   
-    }
-                                  
+  publish_code_command_linux = "az webapp deployment source config-zip --resource-group ${azurerm_linux_web_app.app.resource_group_name} --name ${azurerm_linux_web_app.app.name} --src ${var.archive_file_streamlit}"
+}
 
-
+// This null_resource block publishes code to the Azure Web App (Linux) using the local-exec provisioner.
 resource "null_resource" "app" {
   provisioner "local-exec" {
     command = local.publish_code_command_linux
   }
   depends_on = [local.publish_code_command_linux]
   triggers = {
-    input_json = filemd5(var.archive_file_streamlit)
+    input_json            = filemd5(var.archive_file_streamlit)
     publish_code_command = local.publish_code_command_linux
   }
 }
